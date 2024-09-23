05e84e1feb8115f26231679e01775011202b8862
sudjoao <isudjoao@gmail.com>
2024-09-23 08:58:28 -0300

    build: create project

---
+++ src/main/java/com/sudjoao/song_finder/SongFinderApplication.java
@@ -0,0 +1,13 @@
+package com.sudjoao.song_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudjoao/song_finder/SongFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudjoao.song_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
